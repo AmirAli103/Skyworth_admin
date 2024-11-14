f438f0faba28d17ed214a27680ae6566bec7701b
Amir Ali <aali@techtiz.co>
2024-11-14 14:42:35 +0500

    fixing the build issue

--- pages/SignIn/Index.js
+++ pages/SignIn/Index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Box, Typography, Container, Grid, CircularProgress } from '@mui/material';
 import CustomButton from '../../components/buttons/CustomButton';
 import TextInputField from '../../components/InputField/TextInputField';
-import signInStyles from './SignInStyle';
 import useForm from '../../hooks/useForm';
 
 export default function SignIn() {
@@ -17,6 +16,63 @@ export default function SignIn() {
     handleSubmit,
   } = useForm();
 
+  const signInStyles = {
+    container: {
+      minHeight: '100vh',
+      display: 'flex', // Use flexbox to align items
+    },
+    imageContainer: {
+      backgroundImage: 'url("/signIn.png")',
+      backgroundSize: 'cover', // Change to cover to ensure full coverage
+      backgroundPosition: 'center',
+      minHeight: { xs: '50vh', md: '100vh' },
+      width: '100%', // Adjust the width to 50% for left side
+    },
+    formContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center', // Centers items vertically
+      alignItems: { xs: 'baseline', md: 'baseline' }, // Centers items horizontally
+      padding: 4,
+      width: { xs: '100%', md: '80%' }, // Adjust the width to 50% for right side
+    },
+    title: {
+      color: '#0063B2',
+      fontFamily: "'kanit'",
+      fontSize: { xs: '40px', md: '64px' },
+      fontWeight: 300,
+    },
+    title2: {
+      color: '#0063B2',
+      fontFamily: "'kanit'",
+      fontSize: { xs: '40px', md: '64px' },
+      fontWeight: 500,
+    },
+    form: {
+      width: '100%',
+      maxWidth: { xs: '100%', md: '80%' },
+      alignItems: 'baseline',
+      mt: 3,
+    },
+    checkboxContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      mt: 2,
+    },
+    linkText: {
+      mt: 2,
+      color: 'black',
+      fontFamily: 'kanit',
+      textAlign: 'center',
+    },
+    signUpLink: {
+      color: '#FFD700',
+      cursor: 'pointer',
+    },
+  };
+
   return (
     <Container maxWidth={false} disableGutters sx={signInStyles.container}>
       <Grid container sx={{ minHeight: '100vh' }}>
@@ -25,7 +81,7 @@ export default function SignIn() {
         </Grid>
         <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <Box sx={signInStyles.formContainer}>
-            <Typography sx={{ ...signInStyles.title, textAlign: 'left',marginBottom:'0px' }}>
+            <Typography sx={{ ...signInStyles.title, textAlign: 'left', marginBottom: '0px' }}>
               Hello,
             </Typography>
             <Typography sx={{ ...signInStyles.title2, textAlign: 'left' }}>
@@ -48,22 +104,10 @@ export default function SignIn() {
                 onChange={handleChange}
               />
               {errors.password && <Typography color="error">{errors.password}</Typography>}
-              {/* <RememberMeCheckbox
-                checked={rememberMe}
-                onChange={handleRememberMeChange}
-              /> */}
               <CustomButton type="submit" disabled={loading}>
                 {loading ? <CircularProgress size={24} color="inherit" /> : 'Log In'}
               </CustomButton>
             </Box>
-            {/* <Typography sx={signInStyles.linkText}>
-              Don’t have an account?{' '}
-              <Link href="/signUp" passHref>
-                <Typography component="span" sx={signInStyles.signUpLink}>
-                  Sign Up
-                </Typography>
-              </Link>
-            </Typography> */}
           </Box>
         </Grid>
       </Grid>
